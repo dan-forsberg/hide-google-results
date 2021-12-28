6e9f2789ed80cfa2c6d1cd8f7b9e93ee3d0892fa
Dan Forsberg <dan.s.forsberg@gmail.com>
2021-12-28 15:00:06 +0100

    init

---
+++ script.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Remove shite results
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*.google.com/search*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  const blockedSites = [
+      "https://pretagteam.com",
+      "https://www.codegrepper.com"
+  ];
+
+  let count = 0;
+  blockedSites.forEach((blockedSite) => {
+    const links = document.querySelectorAll(`a[href^='${blockedSite}']`);
+    links.forEach((link) => {
+      link.parentElement.parentElement.remove();
+      count++;
+    });
+  });
+
+  console.log(`Removed ${count} results.`)
+})();
